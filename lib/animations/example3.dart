ed027af93950e0b246c14910908288f9f413fdf8
jerin <jerinjacob@eloit.com>
2023-02-22 18:03:00 +0530

    added a colorless variant in example 3

--- lib/animations/example3.dart
+++ lib/animations/example3.dart
@@ -44,6 +44,7 @@ class _Example3State extends State<Example3> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     _xController
       ..reset()
       ..repeat();
@@ -60,93 +61,143 @@ class _Example3State extends State<Example3> with TickerProviderStateMixin {
       appBar: AppBar(
         title: const Text("Cube Animation"),
       ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation:
-              Listenable.merge([_xController, _yController, _zController]),
-          builder: (context, child) {
-            return Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()
-                ..rotateX(_animation.evaluate(_xController))
-                ..rotateY(_animation.evaluate(_yController))
-                ..rotateZ(_animation.evaluate(_zController)),
-              child: Stack(
-                children: [
-                  // front
-                  Container(
-                    color: Colors.redAccent,
-                    width: widthHeight,
-                    height: widthHeight,
-                    child: const Center(child: Text("1")),
-                  ),
-                  // back
-
-                  //left
-                  Transform(
-                    alignment: Alignment.centerLeft,
-                    transform: Matrix4.identity()..rotateY(pi / 2),
-                    child: Container(
-                      color: Colors.blueAccent,
-                      width: widthHeight,
-                      height: widthHeight,
-                      child: const Center(child: Text("2")),
-                    ),
-                  ),
-
-                  //right
-                  Transform(
-                    alignment: Alignment.centerRight,
-                    transform: Matrix4.identity()..rotateY(-pi / 2),
-                    child: Container(
-                      color: Colors.teal,
-                      width: widthHeight,
-                      height: widthHeight,
-                      child: const Center(child: Text("3")),
-                    ),
-                  ),
-
-                  Transform(
-                    alignment: Alignment.center,
-                    transform: Matrix4.identity()
-                      ..translate(Vector3(0, 0, -widthHeight)),
-                    child: Container(
-                      color: Colors.grey,
-                      width: widthHeight,
-                      height: widthHeight,
-                      child: const Center(child: Text("4")),
-                    ),
-                  ),
-
-                  //top
-                  Transform(
-                    alignment: Alignment.topCenter,
-                    transform: Matrix4.identity()..rotateX(-pi / 2),
-                    child: Container(
-                      color: Colors.orange,
-                      width: widthHeight,
-                      height: widthHeight,
-                      child: const Center(child: Text("5")),
-                    ),
-                  ),
-
-                  //bottom
-                  Transform(
-                    alignment: Alignment.bottomCenter,
-                    transform: Matrix4.identity()..rotateX(pi / 2),
-                    child: Container(
-                      color: Colors.red,
-                      width: widthHeight,
-                      height: widthHeight,
-                      child: const Center(child: Text("6")),
-                    ),
-                  ),
-                ],
+      body: SizedBox(
+        width: screenWidth,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(),
+              CubeBuilder(
+                xController: _xController,
+                yController: _yController,
+                zController: _zController,
+                animation: _animation,
+                widthHeight: widthHeight,
+                withColor: true,
               ),
-            );
-          },
-        ),
+              const Spacer(),
+              CubeBuilder(
+                xController: _xController,
+                yController: _yController,
+                zController: _zController,
+                animation: _animation,
+                widthHeight: widthHeight,
+                withColor: false,
+              ),
+              const Spacer(),
+            ]),
       ),
+    );
+  }
+}
+
+class CubeBuilder extends StatelessWidget {
+  const CubeBuilder({
+    super.key,
+    required AnimationController xController,
+    required AnimationController yController,
+    required AnimationController zController,
+    required Tween<double> animation,
+    required this.widthHeight,
+    required this.withColor,
+  })  : _xController = xController,
+        _yController = yController,
+        _zController = zController,
+        _animation = animation;
+
+  final AnimationController _xController;
+  final AnimationController _yController;
+  final AnimationController _zController;
+  final Tween<double> _animation;
+  final double widthHeight;
+  final bool withColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: Listenable.merge([_xController, _yController, _zController]),
+      builder: (context, child) {
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..rotateX(_animation.evaluate(_xController))
+            ..rotateY(_animation.evaluate(_yController))
+            ..rotateZ(_animation.evaluate(_zController)),
+          child: Stack(
+            children: [
+              // front
+              Container(
+                color: withColor ? Colors.redAccent : null,
+                width: widthHeight,
+                height: widthHeight,
+                child: const Center(child: Text("1")),
+              ),
+              // back
+
+              //left
+              Transform(
+                alignment: Alignment.centerLeft,
+                transform: Matrix4.identity()..rotateY(pi / 2),
+                child: Container(
+                  color: withColor ? Colors.blueAccent : null,
+                  width: widthHeight,
+                  height: widthHeight,
+                  child: const Center(child: Text("2")),
+                ),
+              ),
+
+              //right
+              Transform(
+                alignment: Alignment.centerRight,
+                transform: Matrix4.identity()..rotateY(-pi / 2),
+                child: Container(
+                  color: withColor ? Colors.teal : null,
+                  width: widthHeight,
+                  height: widthHeight,
+                  child: const Center(child: Text("3")),
+                ),
+              ),
+
+              Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..translate(Vector3(0, 0, -widthHeight)),
+                child: Container(
+                  color: withColor ? Colors.grey : null,
+                  width: widthHeight,
+                  height: widthHeight,
+                  child: const Center(child: Text("4")),
+                ),
+              ),
+
+              //top
+              Transform(
+                alignment: Alignment.topCenter,
+                transform: Matrix4.identity()..rotateX(-pi / 2),
+                child: Container(
+                  color: withColor ? Colors.orange : null,
+                  width: widthHeight,
+                  height: widthHeight,
+                  child: const Center(child: Text("5")),
+                ),
+              ),
+
+              //bottom
+              Transform(
+                alignment: Alignment.bottomCenter,
+                transform: Matrix4.identity()..rotateX(pi / 2),
+                child: Container(
+                  color: withColor ? Colors.red : null,
+                  width: widthHeight,
+                  height: widthHeight,
+                  child: const Center(child: Text("6")),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
